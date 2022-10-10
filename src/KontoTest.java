6d8242207bc2377080554d520683af1b7053037d
Magnus Ottosen <magnus@ottosen.dk>
2022-10-10 16:12:33 +0200

    TestForKontoMade
    Test:
    Inserting money
    Remove money
    Going to 0 money
    Going under 0 money

---
+++ src/KontoTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KontoTest {
+
+    @Test
+    void remove100MoneyFrom1000() {
+        Konto konto = new Konto();
+
+        konto.setAmountOfMoney(1000);
+        konto.removeMoney(100);
+
+        assertEquals(konto.getAmountOfMoney(),900);
+    }
+    @Test
+    void remove1000MoneyFrom1000() {
+        Konto konto = new Konto();
+
+        konto.setAmountOfMoney(1000);
+        konto.removeMoney(1000);
+
+        assertEquals(konto.getAmountOfMoney(),0);
+    }
+    @Test
+    void remove2000MoneyFrom1000() {
+        Konto konto = new Konto();
+
+        konto.setAmountOfMoney(1000);
+        konto.removeMoney(2000);
+
+        assertEquals(konto.getAmountOfMoney(),0);
+    }
+    @Test
+    void insert1000To1000(){
+        Konto konto = new Konto();
+
+        konto.setAmountOfMoney(1000);
+        konto.insertMoney(1000);
+
+        assertEquals(konto.getAmountOfMoney(),2000);
+    }
+}
